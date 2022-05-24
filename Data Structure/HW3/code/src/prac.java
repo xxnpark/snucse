60a75854681a1fecf003191108161b5416e108f7
Jaewan Park <jaejae1112@gmail.com>
2022-05-24 14:33:24 +0900

    DS

--- Data Structure/HW3/code/src/prac.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class prac {
-    public static void main(String[] args) {
-        String str = "ab cd^de  d";
-        System.out.println(Arrays.toString(str.split(" ")));
-    }
-}
